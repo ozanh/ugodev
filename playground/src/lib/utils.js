4b97f4bb35c869b92c61e16b44f11f718875d991
Ozan HACIBEKIROGLU <ozan@verigraf.com>
2020-11-25 02:58:23 +0300

    add utils lib

---
+++ playground/src/lib/utils.js
@@ -0,0 +1,14 @@
+function debounce (func, wait) {
+  let timeout
+
+  return function executedFunc (...args) {
+    const later = () => {
+      timeout = null
+      func(...args)
+    }
+    clearTimeout(timeout)
+    timeout = setTimeout(later, wait)
+  }
+}
+
+export { debounce }
